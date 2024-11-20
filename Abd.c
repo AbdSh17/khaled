1a61c939ccb6c5246dd9820a177a37b9d82bb901
AbdSh17 <abdalraheem200417a@gmail.com>
2024-11-21 00:00:32 +0200

    add other loop

--- Abd.c
+++ Abd.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
     int x = 0;
     while (x < 10)
     {
         printf("hi");
+        x++;
     }
-    
+
+    while (x > -9)
+    {
+        printf("hiii");
+        x--;
+    }
 }
