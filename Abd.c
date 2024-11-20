e273d780a0db5c5da2577023d31fc8cf47107869
AbdSh17 <154285986+AbdSh17@users.noreply.github.com>
2024-11-21 00:07:07 +0200

    Update Abd.c

--- Abd.c
+++ Abd.c
@@ -2,16 +2,12 @@
 
 int main()
 {
-    int x = 0;
-    while (x < 10)
-    {
-        printf("hi");
-        x++;
-    }
-
     for (int i = 0 ; i < 0 ; i++ ) 
     {
             printf("hhhh");
     }
 
+
+    
+    
 }
